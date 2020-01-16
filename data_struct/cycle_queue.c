2c6f9a8e8c72f214be47e07a09e1a2c183263fac
xiaosongsong <1120344670@qq.com>
2020-01-16 21:34:13 +0800

    update gitignore

--- data_struct/cycle_queue.c
+++ data_struct/cycle_queue.c
@@ -20,15 +20,52 @@ void InitQueueNode(QueueNode *p)
     p->tail = 0;
 }
 
-void QueueNodePush(QueueNode *p)
+void QueueNodePush(QueueNode *p, int x)
 {
+    if ((p->tail + 1) % MaxSize != p->head)
+    {
+        p->data[p->tail] = x;
+        p->tail = (p->tail + 1) % MaxSize;
+    }
+    else
+    {
+        printf("队满\n");
+    }
 }
+
 void QueueNodePop(QueueNode *p)
 {
+    // 判断空队
+    if (p->head == p->tail)
+        return;
+    p->head = (p->head + 1) % MaxSize;
+}
+
+void ShowQueueNode(QueueNode *p)
+{
+    printf("head is : %d\n", p->head);
+    while (p->head != p->tail)
+    {
+        printf("%d,", p->data[p->head]);
+        p->head = (p->head + 1) % MaxSize;
+    }
 }
 
 int main()
 {
-
+    QueueNode p;
+    InitQueueNode(&p);
+    // printf("%d,%d", p.head, p.tail);
+    for (int i = 1; i < 10; i++)
+        QueueNodePush(&p, i);
+    // ShowQueueNode(&p);
+    // 出队
+    QueueNodePop(&p);
+    QueueNodePop(&p);
+    QueueNodePop(&p);
+    QueueNodePush(&p, 20);
+    QueueNodePush(&p, 21);
+    ShowQueueNode(&p);
+    ShowQueueNode(&p);
     return 0;
 }
