2253bda4a68a842d66e09ddb8557e44d243c387c
xiaosongsong <1120344670@qq.com>
2020-02-21 10:43:03 +0800

    leetcode 637

--- leetcode/6xx/leetcode637.c
+++ leetcode/6xx/leetcode637.c
@@ -25,9 +25,116 @@
 */
 
 #include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
 
+#define MaxSize 20480
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+typedef struct TreeQueue
+{
+    struct TreeNode *val[MaxSize];
+    int head;
+    int tail;
+} TreeQueue;
+
+struct TreeNode *createTree(int *nodelist, int i)
+{
+    struct TreeNode *new;
+    if (nodelist[i] == 0 || i > 7)
+    {
+        return NULL;
+    }
+    else
+    {
+        new = (struct TreeNode *)malloc(sizeof(struct TreeNode));
+        new->val = nodelist[i];
+        new->left = createTree(nodelist, 2 * i);
+        new->right = createTree(nodelist, 2 * i + 1);
+        return new;
+    }
+}
+
+// 初始化队列
+void initQueue(TreeQueue *q)
+{
+    q->tail = 0;
+    q->head = 0;
+}
+
+//  判断队是否为空
+bool Empty(TreeQueue *q)
+{
+    return q->tail == q->head;
+}
+
+bool IsFull(TreeQueue *q)
+{
+    return q->tail == MaxSize;
+}
+// 入队
+void Push(TreeQueue *q, struct TreeNode *p)
+{
+    if (!IsFull(q))
+        q->val[q->tail++] = p;
+}
+
+// 出队
+void Pop(TreeQueue *q, struct TreeNode **p)
+{
+    if (!Empty(q))
+        *p = q->val[q->head++];
+}
+
+double *averageOfLevels(struct TreeNode *root, int *returnSize)
+{
+    int size = 0, count = 0;
+    double sum = 0.0;
+    double *result;
+    result = (double *)malloc(sizeof(double) * MaxSize);
+    struct TreeNode *p;
+    TreeQueue queue;
+    initQueue(&queue);
+    if (root == NULL)
+    {
+        *returnSize = 0;
+    }
+    else
+    {
+        // 此时入栈的是根元素
+        Push(&queue, root);
+    }
+    *returnSize = 0;
+    while (!Empty(&queue))
+    {
+        size = queue.tail - queue.head;
+        count = size;
+        sum = 0.0;
+        while (size)
+        {
+            Pop(&queue, &p);
+            sum += p->val;
+            printf("%d,", p->val);
+            if (p->left)
+                Push(&queue, p->left);
+            if (p->right)
+                Push(&queue, p->right);
+            size--;
+        }
+        result[(*returnSize)++] = sum / count;
+    }
+    return result;
+}
 int main()
 {
-
+    int size = 0;
+    struct TreeNode *root;
+    int node[] = {0, 3, 9, 20, 0, 0, 15, 7};
+    root = createTree(node, 1);
+    averageOfLevels(root, &size);
     return 0;
 }
