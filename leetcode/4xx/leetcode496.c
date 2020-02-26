42ddc8444bfb4e292375df68b0edceaa897602d4
xiaosongsong <1120344670@qq.com>
2020-02-26 19:26:36 +0800

    leetcode 496

--- leetcode/4xx/leetcode496.c
+++ leetcode/4xx/leetcode496.c
@@ -30,9 +30,61 @@ nums1和nums2 的数组大小都不超过1000。
 */
 
 #include <stdio.h>
-
+#include <malloc.h>
+typedef struct Dict
+{
+    int key;
+    int value;
+} Dict;
+int *nextGreaterElement(int *nums1, int nums1Size, int *nums2, int nums2Size, int *returnSize)
+{
+    int *result;
+    Dict *nums2dict;
+    nums2dict = (Dict *)malloc(sizeof(Dict) * nums2Size);
+    result = (int *)malloc(sizeof(int) * nums1Size);
+    result = NULL;
+    if (nums2Size == 0)
+        return NULL;
+    for (int i = 0; i < nums2Size; i++)
+    {
+        for (int j = i + 1; j < nums2Size; j++)
+        {
+            if (nums2[j] > nums2[i])
+            {
+                nums2dict[i].key = nums2[i];
+                nums2dict[i].value = nums2[j];
+                break;
+            }
+            if (j == nums2Size - 1)
+            {
+                nums2dict[i].key = nums2[i];
+                nums2dict[i].value = -1;
+            }
+        }
+    }
+    nums2dict[nums2Size - 1].key = nums2[nums2Size - 1];
+    nums2dict[nums2Size - 1].value = -1;
+    for (int i = 0; i < nums1Size; i++)
+    {
+        for (int j = 0; j < nums2Size; j++)
+        {
+            if (nums1[i] == nums2dict[j].key)
+            {
+                result[i] = nums2dict[j].value;
+            }
+        }
+    }
+    *returnSize = nums1Size;
+    // for (int i = 0; i < nums1Size; i++)
+    // {
+    //     printf("%d", result[i]);
+    // }
+    return result;
+}
 int main()
 {
-
+    int size = 0;
+    int nums1[] = {4, 1, 2}, nums2[] = {1, 3, 4, 2};
+    nextGreaterElement(nums1, 3, nums2, 4, &size);
     return 0;
 }
