65857811d41d4c9a611946de5047ca76189f75ca
xiaosongsong <1120344670@qq.com>
2020-02-26 10:56:57 +0800

    leetcode 682

---
+++ leetcode/6xx/leetcode682.c
@@ -0,0 +1,146 @@
+/*
+@File    :   leetcode682.c
+@Time    :   2020/02/25 20:28:45
+@Author  :   xiaosongsong
+@Desc    :   None
+你现在是棒球比赛记录员。
+给定一个字符串列表，每个字符串可以是以下四种类型之一：
+1.整数（一轮的得分）：直接表示您在本轮中获得的积分数。
+2. "+"（一轮的得分）：表示本轮获得的得分是前两轮有效 回合得分的总和。
+3. "D"（一轮的得分）：表示本轮获得的得分是前一轮有效 回合得分的两倍。
+4. "C"（一个操作，这不是一个回合的分数）：表示您获得的最后一个有效 回合的分数是无效的，应该被移除。
+
+每一轮的操作都是永久性的，可能会对前一轮和后一轮产生影响。
+你需要返回你在所有回合中得分的总和。
+
+示例 1:
+
+输入: ["5","2","C","D","+"]
+输出: 30
+解释:
+第1轮：你可以得到5分。总和是：5。
+第2轮：你可以得到2分。总和是：7。
+操作1：第2轮的数据无效。总和是：5。
+第3轮：你可以得到10分（第2轮的数据已被删除）。总数是：15。
+第4轮：你可以得到5 + 10 = 15分。总数是：30。
+示例 2:
+
+输入: ["5","-2","4","C","D","9","+","+"]
+输出: 27
+解释:
+第1轮：你可以得到5分。总和是：5。
+第2轮：你可以得到-2分。总数是：3。
+第3轮：你可以得到4分。总和是：7。
+操作1：第3轮的数据无效。总数是：3。
+第4轮：你可以得到-4分（第三轮的数据已被删除）。总和是：-1。
+第5轮：你可以得到9分。总数是：8。
+第6轮：你可以得到-4 + 9 = 5分。总数是13。
+第7轮：你可以得到9 + 5 = 14分。总数是27。
+注意：
+
+输入列表的大小将介于1和1000之间。
+列表中的每个整数都将介于-30000和30000之间。
+*/
+
+#include <stdio.h>
+#include <malloc.h>
+#include <stdbool.h>
+#include <string.h>
+typedef struct StackNode
+{
+    int val;
+    struct StackNode *next;
+} StackNode;
+
+bool Empty(StackNode *p)
+{
+    return p == NULL;
+}
+
+StackNode *InitStack(StackNode *p)
+{
+    p = NULL;
+    return p;
+}
+
+StackNode *Push(StackNode *p, int x)
+{
+    StackNode *new;
+    new = (StackNode *)malloc(sizeof(StackNode));
+    new->val = x;
+    if (p == NULL)
+    {
+        new->next = NULL;
+        return new;
+    }
+    else
+    {
+        new->next = p;
+        p = new;
+        return p;
+    }
+}
+
+StackNode *Pop(StackNode *p)
+{
+    if (!Empty(p))
+    {
+        StackNode *q;
+        q = p;
+        p = p->next;
+        free(q);
+        q = NULL;
+    }
+    return p;
+}
+
+void getTopValue(StackNode *p, int *x)
+{
+    if (!Empty(p))
+        *x = p->val;
+}
+int calPoints(char **ops, int opsSize)
+{
+    int first, last;
+    int source = 0, current = 0;
+    StackNode *st;
+    st = InitStack(st);
+    for (int i = 0; i < opsSize; i++)
+    {
+        if (!strcmp(ops[i], "+"))
+        {
+            getTopValue(st, &first);
+            st = Pop(st);
+            getTopValue(st, &last);
+            current = first + last;
+            st = Push(st, first);
+            st = Push(st, current);
+            source += current;
+        }
+        else if (!strcmp(ops[i], "D"))
+        {
+            getTopValue(st, &current);
+            st = Push(st, current * 2);
+            source += current * 2;
+        }
+        else if (!strcmp(ops[i], "C"))
+        {
+            getTopValue(st, &current);
+            st = Pop(st);
+            source -= current;
+        }
+        else
+        {
+            current = atoi(ops[i]);
+            st = Push(st, current);
+            source += current;
+        }
+    }
+    return source;
+}
+int main()
+{
+    char *node[] = {"5", "2", "C", "D", "+"};
+    calPoints(node, 5);
+    return 0;
+}
