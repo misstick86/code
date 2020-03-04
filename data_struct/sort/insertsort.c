ed3cc34fd3cfbea345bb3dae31e365ba23443ea8
xiaosongsong <1120344670@qq.com>
2020-03-04 21:46:46 +0800

    insert sort

---
+++ data_struct/sort/insertsort.c
@@ -0,0 +1,30 @@
+/*
+@File    :   insertsort.c
+@Time    :   2020/03/04 21:10:46
+@Author  :   xiaosongsong
+@Desc    :   None
+
+直接插入排序
+*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int temp, j;
+    int node[] = {9, 10, 38, 25, 13, 2};
+    int n = sizeof(node) / sizeof(int);
+    for (int i = 1; i < n; i++)
+    {
+        temp = node[i];
+        if (temp < node[i - 1])
+        {
+            for (j = i - 1; node[j] > temp; j--)
+            {
+                node[j + 1] = node[j];
+            }
+            node[j + 1] = temp;
+        }
+    }
+    return 0;
+}
