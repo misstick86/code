f0b323353d70b845f35488529c069eb22fdc2105
xiaosongsong <1120344670@qq.com>
2020-03-05 12:17:38 +0800

    shellsort

--- data_struct/sort/insertsort.c
+++ data_struct/sort/insertsort.c
@@ -16,9 +16,10 @@ int main(int argc, char const *argv[])
     int n = sizeof(node) / sizeof(int);
     for (int i = 1; i < n; i++)
     {
-        temp = node[i];
+        temp = node[i]; //辅助空间
         if (temp < node[i - 1])
         {
+            // 从后向前挪动位置
             for (j = i - 1; node[j] > temp; j--)
             {
                 node[j + 1] = node[j];
---
+++ data_struct/sort/shellsort.c
@@ -0,0 +1,36 @@
+/*
+@File    :   shellsort.c
+@Time    :   2020/03/05 11:30:51
+@Author  :   xiaosongsong
+@Desc    :   None
+
+希尔排序
+*/
+
+#include <stdio.h>
+
+void shellseort(int *node, int count, int k)
+{
+    int temp, j; //临时存储
+    for (int i = k; i < count; i = i + k)
+    {
+        temp = node[i];
+        // 每个相隔k距离的位置做一下调换，看成一次插入排序
+        for (j = i - k; j >= 0 && temp < node[j]; j = j - k)
+        {
+            node[j + k] = node[j];
+        }
+        node[j + k] = temp;
+    }
+}
+int main()
+{
+    int node[] = {70, 81, 59, 21, 92, 34, 13, 76, 8, 2, 11, 22, 77, 53, 18, 28};
+    int count = sizeof(node) / sizeof(int);
+    int d[] = {5, 3, 2, 1};
+    for (int i = 0; i < 4; i++)
+    {
+        shellseort(node, count, d[i]);
+    }
+    return 0;
+}
