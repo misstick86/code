09c99eb0736c0626893a7204196772d9c102c7cd
xiaosongsong <1120344670@qq.com>
2020-03-08 20:39:35 +0800

    leetcode 234

--- leetcode/2xx/leetcode234.c
+++ leetcode/2xx/leetcode234.c
@@ -21,14 +21,76 @@
 
 #include <stdio.h>
 #include <malloc.h>
-
+#include <stdbool.h>
 struct ListNode
 {
     int val;
     struct ListNode *next;
 };
+struct ListNode *createList(int *node, int n)
+{
+    struct ListNode *head, *new, *pointer;
+    new = (struct ListNode *)malloc(sizeof(struct ListNode));
+    new->val = node[0];
+    new->next = NULL;
+    head = new;
+    pointer = new;
+    for (int i = 1; i < n; i++)
+    {
+        new = (struct ListNode *)malloc(sizeof(struct ListNode));
+        new->val = node[i];
+        new->next = NULL;
+        pointer->next = new;
+        pointer = new;
+    }
+    return head;
+}
+
+bool isPalindrome(struct ListNode *head)
+{
+    int flag = 0;
+    struct ListNode *first, *last, *p, *pre = NULL;
+    first = head;
+    last = head;
+    if (head == NULL || head->next == NULL)
+        return true;
+    // 找到中间节点
+    while (first && first->next)
+    {
+        first = first->next->next;
+        last = last->next;
+    }
+    if (first)
+        flag = 1;
+    first = last;
+    // 反转链接
+    last = head;
+    while (last != first)
+    {
+        p = last;
+        last = p->next;
+        p->next = pre;
+        pre = p;
+    }
+    if (flag)
+        first = first->next;
+    while (pre && first)
+    {
+        if (pre->val != first->val)
+            return false;
+        pre = pre->next;
+        first = first->next;
+    }
+    if (pre != NULL || first != NULL)
+        return false;
+
+    return true;
+}
 int main()
 {
-    int node[] = {1, 2, 2, 1};
+    struct ListNode *head;
+    int node[] = {1, 2, 3, 3, 3, 2, 1};
+    head = createList(node, 7);
+    printf("%d", isPalindrome(head));
     return 0;
 }
