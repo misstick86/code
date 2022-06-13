6c2313fa4c1d39a43b3595dc87b118c5948ca5da
Song <1120344670@qq.com>
2022-06-13 12:09:09 +0800

    add hello world

--- ebpf_code/hello_world/helloworld.c
+++ ebpf_code/hello_world/helloworld.c
@@ -1,2 +1,12 @@
-#include"comm:wq
-"
+// +build ignore
+
+#include"common.h"
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+
+
+SEC("do_sys_openat2")
+int hello_world() {
+    bpf_trace_printk("hello world");
+}
---
+++ ebpf_code/hello_world/main.go
@@ -0,0 +1,8 @@
+package main
+
+// $BPF_CLANG and $BPF_CFLAGS are set by the Makefile.
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc $BPF_CLANG -cflags $BPF_CFLAGS -target native bpf helloworld.c -- -I../headers
+
+func main() {
+
+}
